d2cda2a829bf1a31b8c6dc6e31b7df13dd07ad0a
ChingShan86 <31542041+ChingShan86@users.noreply.github.com>
2017-09-02 12:07:55 +0800

    測試
    測試添加註記

--- DeepLearning/CNN_cifar-10/cifar.py
+++ DeepLearning/CNN_cifar-10/cifar.py
@@ -6,7 +6,9 @@ Created on Thu Aug 27 11:27:34 2015
 @author: lab-liu.longpo
 """
 
-
+#哈哈2
+#悠悠
+#ㄎㄎ
 from __future__ import absolute_import
 from __future__ import print_function
 from keras.models import Sequential
